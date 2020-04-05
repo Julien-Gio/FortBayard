ced58dd94f34f95c9118d959cbd21b6b242aa557
Julien Giovinazzo <juliengiovinazzo@gmail.com>
2020-04-05 14:34:19 +0200

    Commentaires classe FaceHandler

--- AMazeing/facehandler.h
+++ AMazeing/facehandler.h
@@ -20,70 +20,80 @@ using namespace cv;
 
 class FaceHandler {
     std::thread* th;
-    Maze* maze;  // Passed in constructor. FaceHandler does not create nor delete maze
+    Maze* maze;  // Passer dans le constructeur. FaceHandler ne créer/delete pas maze.
 
+    // Quelques constantes pour rendre le code plus lisible //
+    // Indices des directions dans le tableau SEUILS
     const int HAUT = 0;
     const int DROITE = 1;
     const int BAS = 2;
     const int GAUCHE = 3;
+    const float SEUILS[4] = {3.0, 2.0, 2.0, 2.0};  // Dans l'ordre : Haut, droite, bas, gauche
 
     const float ROTATION_SPEED = 0.01;
     const float WALK_SPEED = 0.05;
 
     const int frameWidth = 320;
     const int frameHeight = 240;
-    const int subImageWidth = 100;
-    const int subImageHeight = 100;
     const float templateWidth = 0.4;  // Percentage of face
     const float templateHeight = 0.5;  // Percentage of face
-    const float templateOffsetX = 0.0;  // Percentage of face. < 0 to the left, > 0 to the right
+    const float templateOffsetX = 0.0;  // Percentage of face. '= 0' centered, '< 0' to the left, '> 0' to the right
     const float templateOffsetY = 0.05;  // Percentage of face. < 0 above center, > 0 below center
 
 
-    const float SEUILS[4] = {3.0, 2.0, 2.0, 2.0};  // Haut, droite, bas, gauche
     const unsigned int NUM_AVG_OVER_FRAMES = 12;  // Nombre de frames du visage en mémoire pour trouver la moyenne
 
-    /* STATES
+    /* MACHINE D'ETAT :
      * 'C' : continously detecting faces
      * 'N' : currently no face is detected
      * 'J' : just detected a face
      *
-     * C / J => n if no face is detected
+     * TRANSITIONS :
+     * C / J => N if no face is detected
      * N => J if a face is detected
      * J => C if faces are detected for long enough
      *
-     * movement calculations are only done in state 'C'
+     * 'C' est le seul etat où les calculs de rotation de la tête sont faits.
     **/
-    const char STATES[3] = {'C', 'J', 'N'};
-    char state;
+    const char STATES[3] = {'C', 'J', 'N'};  // Etats possibles
+    char state;  // Etat actuel
 
-    std::vector<Rect> faces;
+    std::vector<Rect> faces;  // Tableau avec les visages des n frames précedentes (nous permettant de faire un moyenne)
     VideoCapture cap;
-    CascadeClassifier faceCascade;
+    CascadeClassifier faceCascade;  // Objet pouvant trouver un visage
 
     Rect templateRect;
-    Mat templateImage;  // to store the nose
-    Mat resultImage;    // to store the matchTemplate result
+    Mat templateImage;  // Matrice avec le motif (zone autour du nez)
+    Mat resultImage;    // Pour stocker le restulat de la X-corrélation
 
     Mat frame, frameRect;
 
     bool debugGraphics = true;
 
 public:
-    FaceHandler(Maze* maze_);
-    ~FaceHandler();
+    FaceHandler(Maze* maze_);  // Constructeur
+    ~FaceHandler();  // Destructeur
 
+    /* Role : récoupère un visage et appel updateC/J/N en fonction de l'état actuel. Gère les changement d'état aussi. */
     void update();
 
+    /* Role : active ou desactive la visualisation du debuggage. */
     void toogle_debug_graphics() {
         debugGraphics = !debugGraphics;
     }
 
 private:
-    void updateC(Rect& face);  // State 'C'
-    void updateJ(Rect& face);  // State 'J'
-    void updateN(Rect& face);  // State 'N'
+    void updateC(Rect& face);  // Rôle : méthode de mise à jour de l'etat 'C'.
+    void updateJ(Rect& face);  // Rôle : méthode de mise à jour de l'etat 'J'.
+    void updateN(Rect& face);  // Rôle : méthode de mise à jour de l'etat 'N'.
+
+    /* Rôle : Utilise la méthode detectMultiScale de la classe CascadeClassifieur pour extraire le visage de l'image.
+     * Sortie : un Rect donnant la position du visage dans cette frame.
+     * */
     Rect getFace(Mat* frame_gray);
+
+    /* Rôle : calcul la position moyenne du visage sur les n dernières frames.
+     * Sortie : un Rect avec la position moyenne du visage. */
     Rect getAverageFace();
 
 };
